fcd60f094a71206684421cd3332d3b18eb91abb9
MacroMew <33503006+MacroMew@users.noreply.github.com>
2022-01-28 10:45:19 -0500

    Clicking 'register' sends to register page
    - Splits index.html into three separate files: index, login, and register
    - Resolves issue with password overwriting username on form submit
    - Gives links to navbar buttons
    - name.html now, temporarily displays password

--- Web_Application/Web_Application_Example/app/app.py
+++ Web_Application/Web_Application_Example/app/app.py
@@ -1,17 +1,29 @@
 from urllib import request
 from flask import Flask, render_template, request
 app = Flask(__name__)
+classes = ["login", "register"]
+
 
 @app.route('/')
 def index():
-    classes = ["Foo","Bar","Baz"]
     return render_template("index.html",classes=classes)
 
 @app.route('/test_form', methods=['POST'])
 def test_form():
+    #gets the name and password from the POST payload
     name = request.form['name']
-    return render_template("name.html",name=name)
+    password = request.form['password']
+    return render_template("name.html",name=name, password=password)
 
+#function to redirect to register page
+@app.route('/register')
+def register():
+    return render_template("register.html",classes=classes)
+
+#function to redirect to login page
+@app.route('/login')
+def login():
+    return render_template("login.html", classes=classes)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
